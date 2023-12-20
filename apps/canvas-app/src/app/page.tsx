eb059de6d81a7dbac40ac2594510e4ca2b3f30f3
abhi1992002 <abhimanyu1992002@gmail.com>
2023-12-20 20:54:25 +0530

    first-commit

---
+++ apps/canvas-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/primitive/modeToggle";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <Button>Hello</Button>
+      <ModeToggle />
+    </>
+  );
+}
